8555634a7067126af4a2dfe843f08b28b1b2f343
Mike <miheishe@gmail.com>
2017-02-07 22:21:55 +0300

    internal links

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
 	posts = Post.objects.filter().order_by('published_date')
 	return render(request,'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+	post = get_object_or_404(Post, pk = post_id) #pk = primary key or name of field (id for example)
+	return render(request, 'blog/post_detail.html', {'post': post})
